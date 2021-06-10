7df5e1d7d6d1ef29a7cf2270b7075c8d08287b7c
jasonnonstop10 <jay44411@gmail.com>
2021-06-10 16:38:01 +0700

    hello tu

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/kwem', (req, res) => {
+  res.send('nope');
 })
 
-// app.post('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.post('/see/you/leatwe', async (req, res) => {
+   res.send('akjdakdjkal')
+ })
 
-// app.put('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.put('/ma', (req, res) => {
+   res.json({hge:"dsa"})
+})
 
-// app.delete('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.delete('/jae', (req, res) => {
+   await res.send('plas')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
