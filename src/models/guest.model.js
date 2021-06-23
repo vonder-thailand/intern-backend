3ec5d654b37ba00e4bf943b82d7fcd12afac1ce5
chanok <Chanok_boon@hotmail.com>
2021-06-23 17:19:22 +0700

    [ADD] format code

--- src/models/guest.model.js
+++ src/models/guest.model.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const guests = new Schema(
-    {
-        name: { type: String, default: "-" },
-        isDeleted: { type: Boolean, default: false }
-    },
-    {
-        strict: false,
-        versionKey: false,
-        timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
-    }
+  {
+    name: { type: String, default: "-" },
+    isDeleted: { type: Boolean, default: false },
+  },
+  {
+    strict: false,
+    versionKey: false,
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
 );
 
 module.exports = mongoose.model("guests", guests);
--- src/utils/s3.js
+++ src/utils/s3.js
@@ -1,24 +1,32 @@
-
 const path = require("path");
-const fs = require('fs')
+const fs = require("fs");
 const aws = require("aws-sdk");
-const {AWS_BUCKET_NAME,AWS_BUCKET_REGION,AWS_ACCESS_KEY,SECRET_ACCESS_KEY} = process.env
+const {
+  AWS_BUCKET_NAME,
+  AWS_BUCKET_REGION,
+  AWS_ACCESS_KEY,
+  SECRET_ACCESS_KEY,
+} = process.env;
 aws.config.setPromisesDependency();
 aws.config.update({
   accessKeyId: AWS_ACCESS_KEY,
   secretAccessKey: SECRET_ACCESS_KEY,
   region: AWS_BUCKET_REGION,
-  
 });
-const s3  = new aws.S3()
-async function  uploadManyFile(files,userId,pathS3){
-
+const s3 = new aws.S3();
+async function uploadManyFile(files, userId, pathS3) {
   if (mongoose.Types.ObjectId.isValid(userId)) {
     const fileReturn = await Promise.all(
       files.map(async (item, index) => {
-        const filePath = path.join(__dirname, "..","..", "uploads", item.filename);
-        const key = pathS3 + "/" + userId + "/" + item.filename + '.jpeg'
-       
+        const filePath = path.join(
+          __dirname,
+          "..",
+          "..",
+          "uploads",
+          item.filename
+        );
+        const key = pathS3 + "/" + userId + "/" + item.filename + ".jpeg";
+
         var params = {
           Bucket: AWS_BUCKET_NAME,
           Key: key,
@@ -26,38 +34,29 @@ async function  uploadManyFile(files,userId,pathS3){
           ContentType: "image/jpeg",
           ACL: "public-read",
         };
-    
+
         const data = await s3
           .upload(params)
           .promise()
           .then((data) => {
             fs.unlinkSync(filePath);
-    
+
             return data;
           });
-    
+
         return data["Location"];
       })
     );
-    
+
     return fileReturn;
-  }
-    
-   else {
+  } else {
     throw {
       message: "userid is not defined",
       status: 404,
     };
-  
-
-
-
+  }
 }
 
-}
-
-
-exports.uploadManyFile = uploadManyFile
-
+exports.uploadManyFile = uploadManyFile;
 
 //dowmload a file from 23
