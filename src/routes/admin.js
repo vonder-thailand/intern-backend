b6dd0303b4ba9f9fd1009c6469534368cdd7b0bc
JoJo <titichaya_love@hotmail.com>
2021-06-25 12:59:28 +0700

    [FIX] authMiddlewear

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 const auth = require("../middlewares/auth");
 
-router.get("/results", adminController.getAllResult);
+router.get("/results", auth.authMiddleware, adminController.getAllResult);
 router.get("/admin/find", auth.authMiddleware, adminController.getAdminById);
 router.get("/admin", auth.authMiddleware, adminController.getAllAdmins);
 router.post("/questions", auth.authMiddleware, adminController.postQuestion);
-router.get("/questions", adminController.getAllQuestions);
-router.get("/questions/cat", adminController.getQuestionByCat);
-router.put("/update", adminController.updateFields);
+router.get("/questions", auth.authMiddleware, adminController.getAllQuestions);
+router.get(
+  "/questions/cat",
+  auth.authMiddleware,
+  adminController.getQuestionByCat
+);
+router.put("/update", auth.authMiddleware, adminController.updateFields);
 
 module.exports = router;
