e07984a93c0aac865d708ae4c40449f78c3d7640
JoJo <titichaya_love@hotmail.com>
2021-07-01 11:29:46 +0700

    :art: [FIX] guest & object result

--- src/routes/guest.js
+++ src/routes/guest.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 const authController = require("../controllers/authController");
+const guestController = require("../controllers/guestController");
 const auth = require("../middlewares/guestAuth");
 
-router.post(
-  "/guest/result",
-  auth.guestAuthMiddleware,
-  usersController.createResultById
-);
-router.post("/guest", usersController.createGuest);
-router.post("/guest/signup", auth.guestAuthMiddleware, authController.signup);
+// router.post(
+//   "/guest/result",
+//   auth.guestAuthMiddleware,
+//   usersController.createResultById
+// );
+// router.post("/guest", usersController.createGuest);
+// router.post("/guest/signup", auth.guestAuthMiddleware, authController.signup);
+
+router.get("/guest/result", guestController.calculateResult);
+
 module.exports = router;
