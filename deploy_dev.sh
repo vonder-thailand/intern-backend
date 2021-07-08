b668871659e8d369b5861724f386f802f1f43f5d
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-07-08 13:26:18 +0700

    [edit] config.yml,deploy.sh

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 pm2 stop dev  
-pm2 delete dev  
-git pull  
+pm2 delete dev    
 npm install 
 PORT=5000 pm2 start app.js --name dev
--- deploy_staging.sh
+++ deploy_staging.sh
@@ -2,6 +2,5 @@
 
 pm2 stop staging
 pm2 delete staging
-git pull  
 npm install 
 PORT=3000 pm2 start app.js --name staging
