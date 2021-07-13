f48ed94aabec9b4e9ffcc6232a4ec2a24d0e4566
jasonnonstop10 <jay44411@gmail.com>
2021-07-13 14:40:46 +0700

    [add] update route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,15 +6,15 @@ const auth = require("../middlewares/auth");
 router.get("/results", auth.authMiddleware, adminController.getAllResult);
 router.get("/admin/find", auth.authMiddleware, adminController.getAdminById);
 router.get("/admin", auth.authMiddleware, adminController.getAllAdmins);
-router.post("/questions", adminController.postQuestion);
 router.get("/questions", adminController.getAllQuestions);
 router.get(
   "/questions/cat",
   auth.authMiddleware,
   adminController.getQuestionByCat
 );
-router.post("/summarize", auth.authMiddleware, adminController.postSummarise);
 router.get("/summarize", auth.authMiddleware, adminController.getSummarise);
+router.post("/questions", adminController.postQuestion);
+router.post("/summarize", auth.authMiddleware, adminController.postSummarise);
 router.put("/update", auth.authMiddleware, adminController.updateFields);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,65 +4,22 @@ const usersController = require("../controllers/usersController");
 const auth = require("../middlewares/auth");
 const multer = require("multer");
 
-router.post(
-  "/images",
-  auth.authMiddleware,
-  multer({ dest: "uploads/" }).array("photo", 10),
-  usersController.postImage
-);
-
 router.get("/user/find", auth.authMiddleware, usersController.findUserById);
-router.put("/user", auth.authMiddleware, usersController.updateUserById);
-router.delete("/user", auth.authMiddleware, usersController.deleteUserById);
-router.post(
-  "/user/result",
-  auth.authMiddleware,
-  usersController.createResultById
-);
 router.get("/user", auth.authMiddleware, usersController.getAllUsers);
-router.post("/comment", auth.authMiddleware, usersController.postComment);
-router.get("/user/result", auth.authMiddleware, usersController.getResultById);
-router.post("/user/content", auth.authMiddleware, usersController.postContent);
 router.get(
   "/user/content/get",
   auth.authMiddleware,
   usersController.getAllContents
-);
-router.post(
-  "/user/content/tag",
-  auth.authMiddleware,
-  usersController.getSortByTag
-);
-router.put(
-  "/user/content",
-  auth.authMiddleware,
-  usersController.contentIsLiked
 );
 router.get(
   "/user/comment/get/:page-:limit",
   auth.authMiddleware,
   usersController.getCommentByContentId
 );
-router.delete(
-  "/user/comment",
-  auth.authMiddleware,
-  usersController.deleteComment
-);
-router.delete(
-  "/user/content",
-  auth.authMiddleware,
-  usersController.deleteContent
-);
 router.get(
   "/user/search/:keyword",
   auth.authMiddleware,
   usersController.search
-);
-
-router.post(
-  "/user/newResult",
-  auth.authMiddleware,
-  usersController.postNewResult
 );
 router.get(
   "/user/newResult",
@@ -82,5 +39,49 @@ router.get(
 );
 
 router.get("/user/profile", auth.authMiddleware, usersController.getProfile);
+
+router.post(
+  "/images",
+  auth.authMiddleware,
+  multer({ dest: "uploads/" }).array("photo", 10),
+  usersController.postImage
+);
+
+router.post("/comment", auth.authMiddleware, usersController.postComment);
+
+router.post("/user/content", auth.authMiddleware, usersController.postContent);
+
+router.post(
+  "/user/content/tag",
+  auth.authMiddleware,
+  usersController.getSortByTag
+);
+
+router.post(
+  "/user/newResult",
+  auth.authMiddleware,
+  usersController.postNewResult
+);
+
+router.put(
+  "/user/content",
+  auth.authMiddleware,
+  usersController.contentIsLiked
+);
+
+router.put("/user", auth.authMiddleware, usersController.updateUserById);
+
+router.delete("/user", auth.authMiddleware, usersController.deleteUserById);
+
+router.delete(
+  "/user/comment",
+  auth.authMiddleware,
+  usersController.deleteComment
+);
+router.delete(
+  "/user/content",
+  auth.authMiddleware,
+  usersController.deleteContent
+);
 
 module.exports = router;
