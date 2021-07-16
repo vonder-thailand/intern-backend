481484f21177c40668e0591da1a9780e66db49b3
jasonnonstop10 <jay44411@gmail.com>
2021-07-16 19:44:01 +0700

    add post

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ const authController = require("../controllers/authController");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const { tryCatch } = require("../middlewares/tryCatchController");
-
+//post
 router.post("/login", tryCatch(authController.login));
 router.post(
   "/signup",
