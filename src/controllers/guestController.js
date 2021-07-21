e4c24c0b42793a71f75bbee892a46229cbf40b4f
JoJo <titichaya_love@hotmail.com>
2021-07-21 14:30:55 +0700

    [FIX] guest result

--- src/controllers/guestController.js
+++ src/controllers/guestController.js
@@ -1,6 +1,6 @@
 const { calculateResult } = require("../functions/guest");
 
 module.exports.calculateResult = async (req, res, next) => {
-  const result = await calculateResult(req.body.question_data);
+  const result = await calculateResult(req.body);
   res.send(result);
 };
