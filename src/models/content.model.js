c4ca536afd05f9dd8f85f3b046975838b1b0877a
jasonnonstop10 <jay44411@gmail.com>
2021-07-27 22:43:58 +0700

    [add] :art: add default image

--- src/models/content.model.js
+++ src/models/content.model.js
@@ -9,7 +9,11 @@ const contents = new Schema(
     author_id: { type: mongoose.ObjectId, defalut: "-" },
     tag: { type: [String], defalut: ["-"] },
     content_type: { type: String, default: "board", require: true },
-    image: { type: String, default: "-" },
+    image: {
+      type: String,
+      default:
+        "https://vonderbucket1.s3.ap-southeast-1.amazonaws.com/userResult/60d96eb6add0dd4f80dd523e/51bd53bdd1240ce8d5e6569ca58a4c7a.jpeg",
+    },
     isDeleted: { type: Boolean, default: false },
   },
   {
